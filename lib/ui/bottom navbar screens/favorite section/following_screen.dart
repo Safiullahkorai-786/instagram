ab975ef9974a2c82881c77fde9d392641b7878c8
Safiullah Korai <shahzaibth3gr8@gmail.com>
2026-02-21 22:35:59 +0500

    added Favorites Screen's Following tab

---
+++ lib/ui/bottom navbar screens/favorite section/following_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class FollowingScreen extends StatelessWidget {
+  const FollowingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(5),
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 50,
+                  width: 50,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      "https://media.licdn.com/dms/image/v2/D4D03AQEXq2aLTU03sQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1720987699577?e=1773273600&v=beta&t=unhX-BHKUXwhyrHfX77bzCWXf_4TccBHw61OldApiho",
+                    ),
+                  ),
+                ),
+                SizedBox(width: 17),
+
+                Text(
+                  "Safiullah Korai",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                ),
+                Text(
+                  "liked ${index + 1} posts. ",
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  "${index + 1}h",
+                  style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/ui/bottom navbar screens/favorite_screen.dart
+++ lib/ui/bottom navbar screens/favorite_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/ui/bottom%20navbar%20screens/favorite%20section/following_screen.dart';
+import 'package:instagram/ui/bottom%20navbar%20screens/favorite%20section/you_screen.dart';
 
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
@@ -10,6 +12,21 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScreenState extends State<FavoriteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Favorite Screeen")));
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            tabs: [
+              Tab(text: "Following"),
+              Tab(text: "You"),
+            ],
+          ),
+        ),
+
+        body: TabBarView(children: [FollowingScreen(), YouScreen()]),
+      ),
+    );
   }
 }
